1657ce3f952b7322290f63543206023015bc5452
Mark Burggraf <markb@mantisbible.com>
2024-10-31 16:25:26 -0700

    working on safe area for phones

--- src/lib/components/Content.svelte
+++ src/lib/components/Content.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
 	let { children } = $props<{ children: () => unknown }>()
-// style="margin-top: calc(var(--safe-area-inset-top, 0px) + 3.5rem);"
-
 </script>
 
-<main class="h-fit">
-    <div class="container mx-auto mb-16"
-    style="margin-top: calc(var(--safe-area-inset-top, 0px) + 3.5rem);"
+<main class="h-fit overflow-y-auto">
+    <div class="container mx-auto relative"
+    style="margin-top: calc(var(--safe-area-inset-top, 0px) + 3.5rem); margin-bottom: calc(var(--safe-area-inset-bottom, 0px) + 3.5rem);"
     >
-        {@render children()}
+        <div class="fixed top-0 left-0 right-0 h-[var(--safe-area-inset-top,0px)] bg-background z-20"></div>
+        
+        <div class="fixed bottom-0 left-0 right-0 h-[var(--safe-area-inset-bottom,0px)] bg-background z-20"></div>
+        
+        <div class="relative">
+            {@render children()}
+        </div>
     </div>
 </main>
 
