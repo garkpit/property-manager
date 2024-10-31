b9647f28514cd68da0770d09eaf67a4b2fdedd2b
Mark Burggraf <markb@mantisbible.com>
2024-10-31 06:13:55 -0700

    button to test things

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js'
+	function handleClick() {
+		const output = document.getElementById('output')
+		if (output) {
+			if (output.textContent === '') {
+				output.textContent = 'Welcome to SvelteKit'
+			} else {
+				output.textContent = ''
+			}
+		}
+	}
+</script>
+
+<div class="h-screen flex flex-col items-center justify-center">
+	<div class="h-24 flex flex-col items-center justify-center gap-8">
+		<Button onclick={handleClick}>Click me</Button>
+		<div id="output" class="h-6"></div>
+	</div>
+</div>
