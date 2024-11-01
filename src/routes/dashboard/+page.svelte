6881042da8c7195c752108cbdd3cab7f53a7b2bf
Mark Burggraf <markb@mantisbible.com>
2024-11-01 05:51:44 -0700

    basic sidebar

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,30 +1,60 @@
 <script lang="ts">
-    import SidebarPage from '$lib/components/sidebar-page.svelte'
     import Content from '$lib/components/Content.svelte'
     import StatusBar from '@/components/StatusBar.svelte'
     import Navbar from '@/components/Navbar.svelte'
+    import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+    import { Menu } from 'lucide-svelte';
+    import { Button } from "$lib/components/ui/button";
 </script>
 
-<!-- <SidebarPage /> -->
+<Sidebar.Provider>
+    <Sidebar.Root>
+        <Sidebar.Content class="pt-[var(--header-height)]">
+            <Sidebar.Group>
+                <Sidebar.GroupLabel>Navigation</Sidebar.GroupLabel>
+                <Sidebar.GroupContent>
+                    <Sidebar.Menu>
+                        <Sidebar.MenuItem>
+                            <Sidebar.MenuButton>
+                                {#snippet child({ props })}
+                                    <a href="/dashboard" {...props}>
+                                        <span>Dashboard</span>
+                                    </a>
+                                {/snippet}
+                            </Sidebar.MenuButton>
+                        </Sidebar.MenuItem>
+                        <!-- Add more menu items as needed -->
+                    </Sidebar.Menu>
+                </Sidebar.GroupContent>
+            </Sidebar.Group>
+        </Sidebar.Content>
+    </Sidebar.Root>
 
-<div id="contentpage" class="flex flex-col h-fit">
-<Navbar>
-    {#snippet topLeft()}
-        TL
-    {/snippet}
-    {#snippet title()}
-        Title
-    {/snippet}
-    {#snippet topRight()}
-        TR
-    {/snippet}
-</Navbar>
+    <div id="contentpage" class="flex flex-col h-fit">
+        <Navbar>
+            {#snippet topLeft()}
+                <Sidebar.Trigger>
+                    {#snippet child({ props })}
+                        <Button variant="ghost" size="icon" {...props}>
+                            <Menu class="h-5 w-5" />
+                        </Button>
+                    {/snippet}
+                </Sidebar.Trigger>
+            {/snippet}
+            {#snippet title()}
+                Title
+            {/snippet}
+            {#snippet topRight()}
+                TR
+            {/snippet}
+        </Navbar>
 
-<Content>
-    {#snippet children()}
-        <h1>welcome to the dashboard</h1>
-    {/snippet}
-</Content>
+        <Content>
+            {#snippet children()}
+                <h1>welcome to the dashboard</h1>
+            {/snippet}
+        </Content>
 
-<StatusBar />
-</div>
+        <StatusBar />
+    </div>
+</Sidebar.Provider>
