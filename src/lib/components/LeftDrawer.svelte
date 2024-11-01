f302185788ae175122a96a19f5d9c76394907959
Mark Burggraf <markb@mantisbible.com>
2024-11-01 07:10:08 -0700

    new LeftDrawer component

---
+++ src/lib/components/LeftDrawer.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+</script>
+
+<Sidebar.Root>
+    <Sidebar.Content class="pt-[var(--header-height)]">
+        <Sidebar.Group>
+            <Sidebar.GroupLabel>Navigation</Sidebar.GroupLabel>
+            <Sidebar.GroupContent>
+                <Sidebar.Menu>
+                    <Sidebar.MenuItem>
+                        <Sidebar.MenuButton>
+                            {#snippet child({ props })}
+                                <a href="/dashboard" {...props}>
+                                    <span>Dashboard</span>
+                                </a>
+                            {/snippet}
+                        </Sidebar.MenuButton>
+                    </Sidebar.MenuItem>
+                    <!-- Add more menu items as needed -->
+                </Sidebar.Menu>
+            </Sidebar.GroupContent>
+        </Sidebar.Group>
+    </Sidebar.Content>
+</Sidebar.Root>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -2,33 +2,14 @@
     import Content from '$lib/components/Content.svelte'
     import StatusBar from '@/components/StatusBar.svelte'
     import Navbar from '@/components/Navbar.svelte'
+    import LeftDrawer from '$lib/components/LeftDrawer.svelte'
     import * as Sidebar from "$lib/components/ui/sidebar/index.js";
     import { Menu } from 'lucide-svelte';
     import { Button } from "$lib/components/ui/button";
 </script>
 
 <Sidebar.Provider>
-    <Sidebar.Root>
-        <Sidebar.Content class="pt-[var(--header-height)]">
-            <Sidebar.Group>
-                <Sidebar.GroupLabel>Navigation</Sidebar.GroupLabel>
-                <Sidebar.GroupContent>
-                    <Sidebar.Menu>
-                        <Sidebar.MenuItem>
-                            <Sidebar.MenuButton>
-                                {#snippet child({ props })}
-                                    <a href="/dashboard" {...props}>
-                                        <span>Dashboard</span>
-                                    </a>
-                                {/snippet}
-                            </Sidebar.MenuButton>
-                        </Sidebar.MenuItem>
-                        <!-- Add more menu items as needed -->
-                    </Sidebar.Menu>
-                </Sidebar.GroupContent>
-            </Sidebar.Group>
-        </Sidebar.Content>
-    </Sidebar.Root>
+    <LeftDrawer />
 
     <div id="contentpage" class="flex flex-col h-fit">
         <Navbar>
