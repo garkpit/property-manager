9f95e3476ddf55d55a64e04d2f93ad6f07a8880a
Mark Burggraf <markb@mantisbible.com>
2024-11-02 10:03:04 -0700

    actions component: dropdown menu of actions

---
+++ src/lib/components/actions.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+  import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
+  import { Button } from "$lib/components/ui/button";
+  import type { Icon } from "lucide-svelte";
+
+  interface ActionItem {
+    icon: Icon;
+    label: string;
+    onClick?: () => void;
+    separator?: boolean;
+  }
+
+  let {
+    items,
+    triggerIcon: TriggerIcon,
+    buttonClass = "",
+  } = $props<{
+    items: ActionItem[];
+    triggerIcon: any;
+    buttonClass?: string;
+  }>();
+</script>
+
+<DropdownMenu.Root>
+  <DropdownMenu.Trigger>
+    <Button variant="ghost" size="icon" class={buttonClass}>
+      <TriggerIcon class="h-4 w-4" />
+      <span class="sr-only">Open actions menu</span>
+    </Button>
+  </DropdownMenu.Trigger>
+  <DropdownMenu.Content>
+    {#each items as item}
+      {#if item.separator}
+        <DropdownMenu.Separator />
+      {/if}
+      {#if item.groupName}
+        <DropdownMenu.Group>
+          <DropdownMenu.GroupHeading>
+            {item.groupName}
+          </DropdownMenu.GroupHeading>
+          {#each item.groupItems as groupItem}
+            <DropdownMenu.Item onclick={groupItem.onClick}>
+              <div class="flex items-center">
+                <groupItem.icon class="mr-2 h-4 w-4" />
+                {groupItem.label}
+              </div>
+            </DropdownMenu.Item>
+          {/each}
+        </DropdownMenu.Group>
+      {:else}
+        <DropdownMenu.Item onclick={item.onClick}>
+          <div class="flex items-center">
+            <item.icon class="mr-2 h-4 w-4" />
+            {item.label}
+          </div>
+        </DropdownMenu.Item>
+      {/if}
+    {/each}
+  </DropdownMenu.Content>
+  <!--
+  <DropdownMenu.Content align="end" class="w-52">
+    {#each items as item}
+      {#if item.separator}
+        <DropdownMenu.Separator />
+      {/if}
+      <DropdownMenu.Item onclick={item.onClick}>
+        {#if item.icon}
+          <item.icon class="mr-2 size-4" />
+        {/if}
+        {item.label}
+      </DropdownMenu.Item>
+    {/each}
+  </DropdownMenu.Content>-->
+</DropdownMenu.Root>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -4,6 +4,46 @@
   import Navbar from "$lib/components/Navbar.svelte";
   import LeftDrawer from "$lib/components/LeftDrawer.svelte";
   import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+  import { MoreVertical, Settings, CircleHelp, LogOut } from "lucide-svelte";
+  import Actions from "$lib/components/actions.svelte";
+  const actionItems: any[] = [
+    {
+      groupName: "Settings",
+      groupItems: [
+        {
+          icon: Settings,
+          label: "Settings",
+          onClick: () => console.log("Settings clicked"),
+        },
+        {
+          icon: CircleHelp,
+          label: "Help",
+          onClick: () => console.log("Help clicked"),
+        },
+      ],
+    },
+    {
+      groupName: "Information",
+      groupItems: [
+        {
+          icon: Settings,
+          label: "Info 1",
+          onClick: () => console.log("Info 1 clicked"),
+        },
+        {
+          icon: CircleHelp,
+          label: "Info 2 - this is a longer label",
+          onClick: () => console.log("Info 2 clicked"),
+        },
+      ],
+    },
+    {
+      icon: LogOut,
+      label: "Logout",
+      onClick: () => console.log("Logout clicked"),
+      separator: true,
+    },
+  ];
 </script>
 
 <Sidebar.Provider>
@@ -15,6 +55,9 @@
       {/snippet}
       {#snippet Center()}
         Dashboard
+      {/snippet}
+      {#snippet Right()}
+        <Actions items={actionItems} triggerIcon={MoreVertical} />
       {/snippet}
     </Navbar>
 
