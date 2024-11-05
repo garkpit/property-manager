ca7af7e4a744fdc0ebb352fb9327e2ce89c9f26c
Mark Burggraf <markb@mantisbible.com>
2024-11-05 14:58:44 -0800

    remove console logs

--- src/lib/services/supabase.ts
+++ src/lib/services/supabase.ts
@@ -1,12 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 import { decryptData } from '$lib/services/crypto'
-
-// import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
-// import { testEncryptionDecryption } from '$lib/encryption.ts'
-console.log("SUPABASE_URL_ENCRYPTED__", __SUPABASE_URL_ENCRYPTED__);
-console.log("SUPABASE_URL_ENCRYPTION_KEY__", __SUPABASE_URL_ENCRYPTION_KEY__);
-console.log("SUPABASE_ANON_KEY_ENCRYPTED__", __SUPABASE_ANON_KEY_ENCRYPTED__);
-console.log("SUPABASE_ANON_KEY_ENCRYPTION_KEY__", __SUPABASE_ANON_KEY_ENCRYPTION_KEY__);
 export const supabase = createClient(
     decryptData(__SUPABASE_URL_ENCRYPTED__, __SUPABASE_URL_ENCRYPTION_KEY__),
     decryptData(__SUPABASE_ANON_KEY_ENCRYPTED__, __SUPABASE_ANON_KEY_ENCRYPTION_KEY__)
