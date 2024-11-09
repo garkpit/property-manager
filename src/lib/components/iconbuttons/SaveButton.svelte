4d89ab1c0d01c4af76aaecfbee223517852282c8
Mark Burggraf <markb@mantisbible.com>
2024-11-08 16:37:35 -0800

    added component SaveButton

---
+++ src/lib/components/iconbuttons/SaveButton.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Button from "$lib/components/ui/button/button.svelte";
+  import { Check } from "lucide-svelte";
+
+  let { onclick } = $props<{ onclick: () => void }>();
+</script>
+
+<Button variant="ghost" size="icon" {onclick} class="h-9 w-9">
+  <Check class="h-5 w-5" />
+  <span class="sr-only">Save changes</span>
+</Button>
