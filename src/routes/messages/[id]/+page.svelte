c482200edb739e2d668b0230d763145fe2ef1125
Mark Burggraf <markb@mantisbible.com>
2024-11-14 07:09:19 -0800

    message details

--- src/routes/messages/[id]/+page.svelte
+++ src/routes/messages/[id]/+page.svelte
@@ -2,6 +2,9 @@
   import { getMessage } from "$lib/services/messageService.svelte";
   import { page } from "$app/stores";
   import PageTemplate from "$lib/components/PageTemplate.svelte";
+  import { Button } from "$lib/components/ui/button";
+  import { goto } from "$app/navigation";
+  import { ArrowLeft } from "lucide-svelte";
 
   const id = $derived($page.params.id);
   let message = $state<any | null>(null);
@@ -20,14 +23,26 @@
 
 <!-- <PageTemplate {actionItems} /> -->
 <PageTemplate>
-  <!--{#snippet TopLeft()}{/snippet}-->
+  {#snippet TopLeft()}
+    <Button
+      variant="ghost"
+      size="icon"
+      onclick={() => {
+        goto("/messages");
+      }}
+    >
+      <ArrowLeft class="h-4 w-4" />
+    </Button>
+  {/snippet}
+
   {#snippet TopCenter()}
     Message Details
   {/snippet}
+
   <!--{#snippet TopRight()}{/snippet}-->
 
   {#snippet Middle()}
-    <pre>{JSON.stringify(message, null, 2)}</pre>
+    {JSON.stringify(message, null, 2)}
   {/snippet}
 
   <!--{#snippet BottomLeft()}{/snippet}-->
