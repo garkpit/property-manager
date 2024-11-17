7583e60dee933484f4280bdd45c207e31c7bfa46
Mark Burggraf <markb@mantisbible.com>
2024-11-17 06:48:47 -0800

    fixed map so it fills the content area

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -329,8 +329,12 @@
 
 <style>
   .map-container {
-    width: 100%;
-    height: 100%;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: calc(100% + 20px);
   }
 
   :global(.maplibregl-canvas) {
