2cb5bd9f80ac048ebde021a025822016fe8e5479
Mark Burggraf <markb@mantisbible.com>
2024-11-16 18:03:26 -0800

    added new star button to unread messages

---
+++ src/lib/components/iconbuttons/StarNew.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Button from "$lib/components/ui/button/button.svelte";
+  import { Star } from "lucide-svelte";
+  import * as Tooltip from "$lib/components/ui/tooltip";
+
+  let { onclick, classes, iconClasses } = $props<{
+    onclick?: () => void;
+    classes?: string;
+    iconClasses?: string;
+  }>();
+</script>
+
+<Tooltip.Root>
+  <Tooltip.Trigger>
+    <Button variant="ghost" size="icon" {onclick} class="h-9 w-9 {classes}">
+      <Star class="h-5 w-5 {iconClasses}" />
+      <span class="sr-only">New</span>
+    </Button>
+  </Tooltip.Trigger>
+  <Tooltip.Content>
+    <p>New</p>
+  </Tooltip.Content>
+</Tooltip.Root>
--- src/routes/messages/Inbox.svelte
+++ src/routes/messages/Inbox.svelte
@@ -5,6 +5,7 @@
   import { goto } from "$app/navigation";
   import { getUser } from "$lib/services/backend.svelte";
   import { getMessageRefreshCounter } from "$lib/state/messageState.svelte";
+  import StarNew from "$lib/components/iconbuttons/StarNew.svelte";
   const user = $derived(getUser());
   const refreshCounter = $derived(getMessageRefreshCounter());
 
@@ -19,6 +20,7 @@
       console.error(error);
     } else if (data) {
       inboxMessages = data;
+      console.log("inboxMessages", inboxMessages);
     }
   };
 
@@ -43,7 +45,11 @@
           goto(`/messages/${message.id}`);
         }}
       >
-        <Table.Cell class="font-medium">{message.subject}</Table.Cell>
+        <Table.Cell class="font-medium"
+          >{#if message.messages_recipients[0].read_at === null}
+            <StarNew iconClasses="text-red-500 fill-current" />
+          {/if}{message.subject}</Table.Cell
+        >
         <Table.Cell>{message.sender_email}</Table.Cell>
         <Table.Cell>{message.created_at}</Table.Cell>
       </Table.Row>
