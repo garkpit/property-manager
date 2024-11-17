ac07d2976deb9df2694ac015a0d01912d30d21c5
Mark Burggraf <markb@mantisbible.com>
2024-11-17 10:11:35 -0800

    better map search demo

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -5,7 +5,18 @@
   import MapPins from "$lib/components/MapPins.svelte";
   import type maplibregl from "maplibre-gl";
   import { OverpassService } from "$lib/services/overpass";
-
+  import {
+    Coffee,
+    ChefHat,
+    Beer,
+    Martini,
+    Landmark,
+    CircleDollarSign,
+    PillBottle,
+    Hospital,
+    School,
+    CircleParking,
+  } from "lucide-svelte";
   interface MapLocation {
     lat: number;
     lng: number;
@@ -34,21 +45,110 @@
     ),
   );
 
-  const load = async () => {
+  const locatePlaces = async (placeType: string) => {
+    if (!map) return;
+    const center = map.getCenter();
     const overpass = new OverpassService();
     places = await overpass.findNearbyPlaces(
-      { lat: 37.7749, lon: -122.4194 }, // San Francisco coordinates
-      "coffee shop",
-      undefined, // radius parameter (using default)
-      3, // limit parameter - show 25 results
+      { lat: center.lat, lon: center.lng },
+      placeType,
+      2000, // radius parameter (using default)
+      10, // limit parameter - show 25 results
     );
+    console.log("places", places);
   };
-  $effect(() => {
-    load();
-  });
+
+  /**
+            "bar": "bar",
+            "pub": "pub",
+            "atm": "atm",
+            "bank": "bank",
+            "pharmacy": "pharmacy",
+            "hospital": "hospital",
+            "school": "school",
+            "parking": "parking",
+
+   */
+  const actionItems: any[] = [
+    {
+      groupName: "Locate:",
+      groupItems: [
+        {
+          icon: Coffee,
+          label: "cafe",
+          onClick: () => {
+            locatePlaces("cafe");
+          },
+        },
+        {
+          icon: ChefHat,
+          label: "restaurant",
+          onClick: () => {
+            locatePlaces("restaurant");
+          },
+        },
+        {
+          icon: Martini,
+          label: "pub",
+          onClick: () => {
+            locatePlaces("pub");
+          },
+        },
+        {
+          icon: Beer,
+          label: "bar",
+          onClick: () => {
+            locatePlaces("bar");
+          },
+        },
+        {
+          icon: CircleDollarSign,
+          label: "atm",
+          onClick: () => {
+            locatePlaces("atm");
+          },
+        },
+        {
+          icon: Landmark,
+          label: "bank",
+          onClick: () => {
+            locatePlaces("bank");
+          },
+        },
+        {
+          icon: PillBottle,
+          label: "pharmacy",
+          onClick: () => {
+            locatePlaces("pharmacy");
+          },
+        },
+        {
+          icon: Hospital,
+          label: "hospital",
+          onClick: () => {
+            locatePlaces("hospital");
+          },
+        },
+        {
+          icon: School,
+          label: "school",
+          onClick: () => {
+            locatePlaces("school");
+          },
+        },
+        {
+          icon: CircleParking,
+          label: "parking",
+          onClick: () => {
+            locatePlaces("parking");
+          },
+        },
+      ],
+    },
+  ];
 </script>
 
-<PageTemplate>
+<PageTemplate {actionItems}>
   {#snippet TopCenter()}
     Interactive Map
   {/snippet}
